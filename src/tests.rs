89abcdab2a532ec5d63bb1bda1c56b980d1cd148
John Spickes <john@spickes.net>
2020-01-30 07:59:16 -0500

    Modify test so that we stay connected for 10 seconds

--- src/tests.rs
+++ src/tests.rs
@@ -1,6 +1,17 @@
 use crate::rllkrpc::Rpc;
+use std::thread::sleep;
+use std::time::Duration;
 
 #[test]
 fn connects_ok() {
+
+    println!("Connecting...");
+
     let _rpc = Rpc::connect("10.0.0.126:50000").unwrap();
+
+    println!("Connected!");
+
+    sleep(Duration::new(10,0));
+
+    println!("Disconnecting!");
 }
