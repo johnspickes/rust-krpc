f2cacdab046a8a5d1b952ab57c576e1bf494bd19
John Spickes <john@spickes.net>
2020-09-22 07:15:04 -0400

    Work in progress

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@
 /// This module is a work in progress, planned to implement the low-level interfacing with kRPC
 /// through protobufs.  
 
+extern crate protobuf;
+
+mod krpc;
+
 pub mod rllkrpc {
 
     use std::net::TcpStream;
@@ -16,6 +20,18 @@ pub mod rllkrpc {
         /// Connect to the kRPC server
         pub fn connect<A: ToSocketAddrs>(addr: A) -> std::io::Result<Krpc> {
             let stream = TcpStream::connect(addr)?;
+
+            // TCP is now connected.
+            // Send a connection request message
+            let cr = krpc::ConnectionRequest {
+                field_type: krpc::ConnectionRequest_Type::RPC,
+                client_name: String::from("rkrpc"),
+                client_identifier: vec![1],
+            };
+
+            let mut os = protobuf::CodedOutputStream::new(&mut stream);
+            os.write_message(cr);
+
             Result::Ok(Krpc {
                 tcp : stream
             })
